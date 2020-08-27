d1ef94cd29cfba9d68f01503d9bf8146cd102892
Jayne <huzijia@217.89.119.52.naturalwireless.com>
2020-08-27 19:04:46 -0400

    Basic quote project.

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+      <app-header :quoteCount="quotes.length" :maxQuote="this.maxQuote"> </app-header>
+      <app-new-quote @quoteAdded="newQuote"> </app-new-quote>
+      <app-quote-grid class="quotes-item" :quotes='this.quotes' @quoteDeleted="deleteQuote"> </app-quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info"> Info: Click on a quote to delete it!</div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+  import Header from './components/Header.vue';
+
+  export default {
+    data: function(){
+      return {
+        quotes : [
+          'Just a quote to see something'
+        ],
+        maxQuote : 10
+      }
+    },
+    methods:{
+      newQuote(quote){
+        if(this.quotes.length >= this.maxQuote){
+          alert("Delete quotes before add a new one!");
+        }
+        else{
+          this.quotes.push(quote);
+        }
+      },
+      deleteQuote(index){
+        this.quotes.splice(index,1);
+      }
+    },
+    components: {
+      appQuoteGrid : QuoteGrid,
+      appNewQuote : NewQuote,
+      appHeader : Header
+    }
+  }
+</script>
+
+<style>
+</style>
